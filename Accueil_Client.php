a166c91659a5bbf3cfc758dfcbc1c085ae93831f
BalthaBF <balthazar.bertrandfoliot@edu.ece.fr>
2025-05-26 13:26:25 +0200

    correction noms

--- Accueil_Client.php
+++ Accueil_Client.php
@@ -14,7 +14,7 @@ session_start();
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="icon" href="Images/Logo_icone.ico" type="image/x-icon">
+    <link rel="icon" href="C:\wamp64\www\Medicare\Images\Logo_icone.ico" type="image/x-icon">
 
     <!-- Bibliothèque jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
